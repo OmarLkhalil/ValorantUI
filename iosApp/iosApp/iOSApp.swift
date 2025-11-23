590f2463ce5512eb42813b263ea19e1663cb4fa4
omarlkhail <omarkkhalil12@gmail.com>
2025-11-23 02:15:17 +0200

    feat: Add iOS compatibility with DataStore implementation and SafeApiCall fixes

---
+++ iosApp/iosApp/iOSApp.swift
@@ -0,0 +1,38 @@
+import UIKit
+import SwiftUI
+import ComposeApp
+
+@main
+struct iOSApp: App {
+    init() {
+        // Initialize Koin for iOS
+        KoinIosKt.initKoinIos()
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+
+    var body: some View {
+        ComposeView()
+            .ignoresSafeArea(.keyboard)
+            .preferredColorScheme(nil) // Allow system theme switching
+    }
+}
+
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainViewControllerKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        // Update for theme changes if needed
+    }
+}
+
